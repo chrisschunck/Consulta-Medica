92142c58134376a2f2e722b86c171ea67b4feea8
Christian Schunck de Almeida <christian.schunck10@gmail.com>
2026-07-21 13:28:01 -0300

    Update PacienteJpaRepository.java

--- src/main/java/br/com/agendamento/adapters/out/persistence/PacienteJpaRepository.java
+++ src/main/java/br/com/agendamento/adapters/out/persistence/PacienteJpaRepository.java
@@ -3,5 +3,6 @@ package br.com.agendamento.adapters.out.persistence;
 import br.com.agendamento.domain.model.PacienteJava;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Schema(description="interface Paciente para repository Spring Data JPA")
 public interface PacienteJpaRepository extends JpaRepository<PacienteJava, Long> {
 }
