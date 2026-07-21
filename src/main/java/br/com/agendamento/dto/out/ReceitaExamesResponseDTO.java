b90ec639a62b93a0b2276df9f096697c7ec43af1
Christian Schunck de Almeida <christian.schunck10@gmail.com>
2026-07-21 12:38:30 -0300

    Add schema annotations to ReceitaExamesResponseDTO

--- src/main/java/br/com/agendamento/dto/out/ReceitaExamesResponseDTO.java
+++ src/main/java/br/com/agendamento/dto/out/ReceitaExamesResponseDTO.java
@@ -5,6 +5,8 @@ import lombok.Data;
 @Data
 public class ReceitaExamesResponseDTO {
     private Long id;
+    @Schema(description="pedido de exames", example="Urina, abdomen total")
     private String exame;
+    @Schema(description="descritivo do pedido de exames", example="Exame de urina, ir de bexiga cheia")
     private String descricao;
 }
