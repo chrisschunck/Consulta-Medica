8a0b516a28a491ff937c4025375a3f14a5faf469
Christian Schunck de Almeida <christian.schunck10@gmail.com>
2026-07-21 13:28:20 -0300

    Update ReceitaExamesJpaRepository.java

--- src/main/java/br/com/agendamento/adapters/out/persistence/ReceitaExamesJpaRepository.java
+++ src/main/java/br/com/agendamento/adapters/out/persistence/ReceitaExamesJpaRepository.java
@@ -3,5 +3,6 @@ package br.com.agendamento.adapters.out.persistence;
 import br.com.agendamento.domain.model.ReceitaExamesJava;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Schema(description="interface ReceitaExames para repository Spring Data JPA")
 public interface ReceitaExamesJpaRepository extends JpaRepository<ReceitaExamesJava, Long> {
 }
