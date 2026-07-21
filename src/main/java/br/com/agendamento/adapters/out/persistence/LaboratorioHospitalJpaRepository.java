de6a430654d76dd5ebfb5e6f999ff0d7482c22bf
Christian Schunck de Almeida <christian.schunck10@gmail.com>
2026-07-21 13:27:13 -0300

    Update LaboratorioHospitalJpaRepository.java

--- src/main/java/br/com/agendamento/adapters/out/persistence/LaboratorioHospitalJpaRepository.java
+++ src/main/java/br/com/agendamento/adapters/out/persistence/LaboratorioHospitalJpaRepository.java
@@ -3,5 +3,6 @@ package br.com.agendamento.adapters.out.persistence;
 import br.com.agendamento.domain.model.LaboratorioHospitalJava;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Schema(description="interface LaboratorioHospital para repository Spring Data JPA")
 public interface LaboratorioHospitalJpaRepository extends JpaRepository<LaboratorioHospitalJava, Long> {
 }
