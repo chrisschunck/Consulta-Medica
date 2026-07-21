21eafa99b53d16c476de0a5344cd74079453d625
Christian Schunck de Almeida <christian.schunck10@gmail.com>
2026-07-21 13:13:58 -0300

    Update ReceitaExamesController.java

--- src/main/java/br/com/agendamento/adapters/in/web/ReceitaExamesController.java
+++ src/main/java/br/com/agendamento/adapters/in/web/ReceitaExamesController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/receitas-exames")
+@Schema(description="classe ReceitaExames para requisições HTTP")
 public class ReceitaExamesController {
 
     private final ReceitaExamesUseCase useCase;
