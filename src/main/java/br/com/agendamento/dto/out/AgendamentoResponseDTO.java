5c312c75f50bf2780537ca855604dcecf21a5f4b
Christian Schunck de Almeida <christian.schunck10@gmail.com>
2026-07-21 12:17:49 -0300

    Enhance AgendamentoResponseDTO with schema descriptions
    Added schema annotations for improved API documentation.

--- src/main/java/br/com/agendamento/dto/out/AgendamentoResponseDTO.java
+++ src/main/java/br/com/agendamento/dto/out/AgendamentoResponseDTO.java
@@ -10,9 +10,14 @@ import java.time.LocalDateTime;
 @Data
 public class AgendamentoResponseDTO {
     private Long id;
+    @Schema(description="nome do Médico agendado", example="Cardiologista")
     private MedicoJava medicoJava;
+    @Schema(description="nome do Paciente agendado", example="Matheus")
     private PacienteJava pacienteJava;
+    @Schema(description="horário de agendado", example="14:00")
     private LocalDateTime horarioAgendado;
+    @Schema(description="local de realizacao de exames", example="Cardiologia - Mooca")
     private ConsultorioJava consultorioJava;
+    @Schema(description="descritivo do agendamento" example="Exame de Urina, ir com a bexiga cheia")
     private String descricao;
 }
