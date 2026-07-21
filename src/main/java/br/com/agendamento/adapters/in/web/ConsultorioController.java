6748309f1b3fa449ee9a2c0fd0db7d628cfe2c4f
Christian Schunck de Almeida <christian.schunck10@gmail.com>
2026-07-21 13:19:27 -0300

    Update ConsultorioController.java

--- src/main/java/br/com/agendamento/adapters/in/web/ConsultorioController.java
+++ src/main/java/br/com/agendamento/adapters/in/web/ConsultorioController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/consultorios")
+@Schema(description="classe Consultorio para requisições HTTP")
 public class ConsultorioController {
 
     private final ConsultorioUseCase useCase;
