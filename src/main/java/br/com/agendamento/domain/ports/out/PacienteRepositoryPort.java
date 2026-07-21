6746517d11e7264b052b313a37bd7459b30cab07
Christian Schunck de Almeida <christian.schunck10@gmail.com>
2026-07-21 13:11:15 -0300

    Update PacienteRepositoryPort.java

--- src/main/java/br/com/agendamento/domain/ports/out/PacienteRepositoryPort.java
+++ src/main/java/br/com/agendamento/domain/ports/out/PacienteRepositoryPort.java
@@ -5,6 +5,7 @@ import br.com.agendamento.domain.model.PacienteJava;
 import java.util.List;
 import java.util.Optional;
 
+@Schema(description="interface Paciente para saida em port repository")
 public interface PacienteRepositoryPort {
 
     PacienteJava salvar(PacienteJava entidade);
