6a9b6c8f469a6d51be76fbb838e8a7e345ac7ede
Christian Schunck de Almeida <christian.schunck10@gmail.com>
2026-07-21 13:27:41 -0300

    Update MedicoJpaRepository.java

--- src/main/java/br/com/agendamento/adapters/out/persistence/MedicoJpaRepository.java
+++ src/main/java/br/com/agendamento/adapters/out/persistence/MedicoJpaRepository.java
@@ -3,5 +3,6 @@ package br.com.agendamento.adapters.out.persistence;
 import br.com.agendamento.domain.model.MedicoJava;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Schema(description="interface Medico para repository Spring Data JPA")
 public interface MedicoJpaRepository extends JpaRepository<MedicoJava, Long> {
 }
