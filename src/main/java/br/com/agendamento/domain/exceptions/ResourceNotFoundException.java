f758b05ef1b326dbda2efbce8dcc9c93238be8ec
Christian Schunck de Almeida <christian.schunck10@gmail.com>
2026-07-21 13:00:04 -0300

    Update ResourceNotFoundException.java

--- src/main/java/br/com/agendamento/domain/exceptions/ResourceNotFoundException.java
+++ src/main/java/br/com/agendamento/domain/exceptions/ResourceNotFoundException.java
@@ -1,5 +1,6 @@
 package br.com.agendamento.domain.exceptions;
 
+@Schema(description="classe ResourceNotFoundException extendida com RuntimeException")
 public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String message){
         super(message);
