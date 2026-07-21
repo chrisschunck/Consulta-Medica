b982be9dce0317577f689c92e096117945459041
Christian Schunck de Almeida <christian.schunck10@gmail.com>
2026-07-21 13:25:02 -0300

    Update AgendamentoExameJpaRepository.java

--- src/main/java/br/com/agendamento/adapters/out/persistence/AgendamentoExameJpaRepository.java
+++ src/main/java/br/com/agendamento/adapters/out/persistence/AgendamentoExameJpaRepository.java
@@ -3,5 +3,6 @@ package br.com.agendamento.adapters.out.persistence;
 import br.com.agendamento.domain.model.AgendamentoExameJava;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Schema(description="interface AgendamentoExame para repository Spring Data JPA")
 public interface AgendamentoExameJpaRepository extends JpaRepository<AgendamentoExameJava, Long> {
 }
