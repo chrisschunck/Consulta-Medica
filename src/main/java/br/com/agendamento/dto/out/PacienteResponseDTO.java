8a8de3f6a3267d9777be3f4f2faef466b04156da
Christian Schunck de Almeida <christian.schunck10@gmail.com>
2026-07-21 12:36:54 -0300

    Update PacienteResponseDTO.java

--- src/main/java/br/com/agendamento/dto/out/PacienteResponseDTO.java
+++ src/main/java/br/com/agendamento/dto/out/PacienteResponseDTO.java
@@ -6,9 +6,14 @@ import lombok.Data;
 public class PacienteResponseDTO {
 
     private Long id;
+    @Schema(description="nome do Paciente", example="Matheus")
     private String nome;
+    @Schema(description="idade do Paciente em número inteiro", example="32")
     private Integer idade;
+    @Schema(description="email de contato do Paciente", example="matheus.paciente@gmail.com")
     private String email;
+    @Schema(description="cpf do paciente para cadastro", example="93293819318")
     private String cpf;
+    @Schema(description="Meio de contato do paciente", example="um número, email")
     private String contato;
 }
