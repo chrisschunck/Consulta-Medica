5b8c3598b8872e2969c0fea34af31a9fb5d5f4e5
Christian Schunck de Almeida <christian.schunck10@gmail.com>
2026-07-21 12:21:22 -0300

    Update ConsultorioResponseDTO.java

--- src/main/java/br/com/agendamento/dto/out/ConsultorioResponseDTO.java
+++ src/main/java/br/com/agendamento/dto/out/ConsultorioResponseDTO.java
@@ -5,6 +5,8 @@ import lombok.Data;
 @Data
 public class ConsultorioResponseDTO {
     private Long id;
+    @Schema(description="nome do Consultorio", example="Cardiologia - Mooca")
     private String nome;
+    @Schema(description="local de realização", example="Rua Mooca, 123")
     private String local;
 }
