6a6b603c409be553d1bf12f20b5c901e73c5fe1a
Christian Schunck de Almeida <christian.schunck10@gmail.com>
2026-07-21 13:26:54 -0300

    Update ConvenioMedicoJpaRepository.java

--- src/main/java/br/com/agendamento/adapters/out/persistence/ConvenioMedicoJpaRepository.java
+++ src/main/java/br/com/agendamento/adapters/out/persistence/ConvenioMedicoJpaRepository.java
@@ -3,5 +3,6 @@ package br.com.agendamento.adapters.out.persistence;
 import br.com.agendamento.domain.model.ConvenioMedicoJava;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Schema(description="interface ConvenioMedico para repository Spring Data JPA")
 public interface ConvenioMedicoJpaRepository extends JpaRepository<ConvenioMedicoJava, Long> {
 }
