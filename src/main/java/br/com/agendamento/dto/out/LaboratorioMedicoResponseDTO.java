a806b5f08072c372dde5dfda9a8225156add1f6d
Christian Schunck de Almeida <christian.schunck10@gmail.com>
2026-07-21 12:44:42 -0300

    Update LaboratorioMedicoResponseDTO.java

--- src/main/java/br/com/agendamento/dto/out/LaboratorioMedicoResponseDTO.java
+++ src/main/java/br/com/agendamento/dto/out/LaboratorioMedicoResponseDTO.java
@@ -3,7 +3,7 @@ package br.com.agendamento.dto.out;
 import lombok.Data;
 
 @Data
-@Schema(description="Classe response dto Laboratorio")
+@Schema(description="Classe response dto LaboratorioMedico")
 public class LaboratorioMedicoResponseDTO {
 
     private Long id;
