ed334b317834d12b07a0c72a28a8f8d136af4c14
Christian Schunck de Almeida <christian.schunck10@gmail.com>
2026-07-21 13:06:59 -0300

    Update MedicoUseCase.java

--- src/main/java/br/com/agendamento/domain/ports/in/MedicoUseCase.java
+++ src/main/java/br/com/agendamento/domain/ports/in/MedicoUseCase.java
@@ -5,6 +5,7 @@ import br.com.agendamento.domain.model.MedicoJava;
 import jakarta.validation.Valid;
 import java.util.List;
 
+@Schema(description="interface Medico para casos de uso")
 public interface MedicoUseCase {
 
     MedicoJava salvar(@Valid MedicoJava entidade);
