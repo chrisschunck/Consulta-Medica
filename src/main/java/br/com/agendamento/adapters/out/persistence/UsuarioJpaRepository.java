92832986e841737de198417bdcf979747418e790
Christian Schunck de Almeida <christian.schunck10@gmail.com>
2026-07-21 13:28:37 -0300

    Update UsuarioJpaRepository.java

--- src/main/java/br/com/agendamento/adapters/out/persistence/UsuarioJpaRepository.java
+++ src/main/java/br/com/agendamento/adapters/out/persistence/UsuarioJpaRepository.java
@@ -4,6 +4,7 @@ import br.com.agendamento.domain.model.UsuarioJava;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
+@Schema(description="interface Usuario para repository Spring Data JPA")
 public interface UsuarioJpaRepository extends JpaRepository<UsuarioJava, Long> {
     Optional<UsuarioJava> findByEmail(String email);
     boolean existsByEmail(String email);
