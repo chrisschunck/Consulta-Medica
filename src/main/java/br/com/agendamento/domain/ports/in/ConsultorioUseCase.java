78aa05cec2bc4d086d1955a4abf7a310d15e3d39
Christian Schunck de Almeida <christian.schunck10@gmail.com>
2026-07-21 13:08:42 -0300

    Update ConsultorioUseCase.java

--- src/main/java/br/com/agendamento/domain/ports/in/ConsultorioUseCase.java
+++ src/main/java/br/com/agendamento/domain/ports/in/ConsultorioUseCase.java
@@ -5,6 +5,7 @@ import br.com.agendamento.domain.model.ConsultorioJava;
 import jakarta.validation.Valid;
 import java.util.List;
 
+@Schema(description="interface Consultorio para casos de uso")
 public interface ConsultorioUseCase {
 
     ConsultorioJava salvar(@Valid ConsultorioJava entidade);
