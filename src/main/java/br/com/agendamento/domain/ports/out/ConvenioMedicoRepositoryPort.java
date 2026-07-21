90ac92bbdc8167cadb934b6e5e42b173d4ee64e3
Christian Schunck de Almeida <christian.schunck10@gmail.com>
2026-07-21 13:12:03 -0300

    Update ConvenioMedicoRepositoryPort.java

--- src/main/java/br/com/agendamento/domain/ports/out/ConvenioMedicoRepositoryPort.java
+++ src/main/java/br/com/agendamento/domain/ports/out/ConvenioMedicoRepositoryPort.java
@@ -5,6 +5,7 @@ import br.com.agendamento.domain.model.ConvenioMedicoJava;
 import java.util.List;
 import java.util.Optional;
 
+@Schema(description="interface ConvenioMedico para saida em port repository")
 public interface ConvenioMedicoRepositoryPort {
 
     ConvenioMedicoJava salvar(ConvenioMedicoJava entidade);
