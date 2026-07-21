addb2e9c21191466326f1f10ae3deff0d78d2779
Christian Schunck de Almeida <christian.schunck10@gmail.com>
2026-07-21 12:25:04 -0300

    Update LaboratorioMedicoResponseDTO.java

--- src/main/java/br/com/agendamento/dto/out/LaboratorioMedicoResponseDTO.java
+++ src/main/java/br/com/agendamento/dto/out/LaboratorioMedicoResponseDTO.java
@@ -6,6 +6,8 @@ import lombok.Data;
 public class LaboratorioMedicoResponseDTO {
 
     private Long id;
+    @Schema(description="nome do local", example="Hospital Albert Eisten")
     private String nome;
+    @Schema(description="local de realização", example="Rua Mooca, 123")
     private String local;
 }
