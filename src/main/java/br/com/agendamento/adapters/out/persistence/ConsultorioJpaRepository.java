362991e2c73bc578189057ce752953709b09dbeb
Christian Schunck de Almeida <christian.schunck10@gmail.com>
2026-07-21 13:26:35 -0300

    Update ConsultorioJpaRepository.java

--- src/main/java/br/com/agendamento/adapters/out/persistence/ConsultorioJpaRepository.java
+++ src/main/java/br/com/agendamento/adapters/out/persistence/ConsultorioJpaRepository.java
@@ -3,5 +3,6 @@ package br.com.agendamento.adapters.out.persistence;
 import br.com.agendamento.domain.model.ConsultorioJava;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Schema(description="interface Consultorio para repository Spring Data JPA")
 public interface ConsultorioJpaRepository extends JpaRepository<ConsultorioJava, Long> {
 }
