03c76ff04bd707d1bffbbef05964d7e432718962
Christian Schunck de Almeida <christian.schunck10@gmail.com>
2026-07-21 13:26:13 -0300

    Update AgendamentoJpaRepository.java

--- src/main/java/br/com/agendamento/adapters/out/persistence/AgendamentoJpaRepository.java
+++ src/main/java/br/com/agendamento/adapters/out/persistence/AgendamentoJpaRepository.java
@@ -3,5 +3,6 @@ package br.com.agendamento.adapters.out.persistence;
 import br.com.agendamento.domain.model.AgendamentoJava;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Schema(description="interface Agendamento para repository Spring Data JPA")
 public interface AgendamentoJpaRepository extends JpaRepository<AgendamentoJava, Long> {
 }
