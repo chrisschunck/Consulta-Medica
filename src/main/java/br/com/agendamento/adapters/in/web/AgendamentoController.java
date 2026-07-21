08dba413cbad927011cd2c70a38c0eb68577afee
Christian Schunck de Almeida <christian.schunck10@gmail.com>
2026-07-21 13:23:16 -0300

    Update AgendamentoController.java

--- src/main/java/br/com/agendamento/adapters/in/web/AgendamentoController.java
+++ src/main/java/br/com/agendamento/adapters/in/web/AgendamentoController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/agendamentos")
+@Schema(description="classe Agendamento para requisições HTTP")
 public class AgendamentoController {
 
     private final AgendamentoUseCase useCase;
