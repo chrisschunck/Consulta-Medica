d25f10441e7a8a0840277d06988fe144417e9bbc
Christian Schunck de Almeida <christian.schunck10@gmail.com>
2026-07-22 13:00:46 -0300

    Update AgendamentoResponseDTO.java

--- src/main/java/br/com/agendamento/dto/out/AgendamentoResponseDTO.java
+++ src/main/java/br/com/agendamento/dto/out/AgendamentoResponseDTO.java
@@ -11,14 +11,14 @@ import java.time.LocalDateTime;
 @Schema(description="classe response dto Agendamento")
 public class AgendamentoResponseDTO {
     private Long id;
-    @Schema(description="nome do Médico agendado", example="Cardiologista")
+    @Schema(description="nome do Médico agendado", example="Cardiologista", minLength= 5, maxLength= 50)
     private MedicoJava medicoJava;
-    @Schema(description="nome do Paciente agendado", example="Matheus")
+    @Schema(description="nome do Paciente agendado", example="Matheus", minLength= 3, maxLength= 50)
     private PacienteJava pacienteJava;
-    @Schema(description="horário de agendado", example="14:00")
+    @Schema(description="horário de agendado", example="14:00", minLength= 4, maxLength= 5)
     private LocalDateTime horarioAgendado;
-    @Schema(description="local de realizacao de exames", example="Cardiologia - Mooca")
+    @Schema(description="local de realizacao de exames", example="Cardiologia - Mooca", minLength= 5, maxLength= 50)
     private ConsultorioJava consultorioJava;
-    @Schema(description="descritivo do agendamento" example="Exame de Urina, ir com a bexiga cheia")
+    @Schema(description="descritivo do agendamento" example="Exame de Urina, ir com a bexiga cheia", minLength= 5, maxLength= 150)
     private String descricao;
 }
