fab515b655954b22825b5f5f6c5728d622cfebbf
Christian Schunck de Almeida <christian.schunck10@gmail.com>
2026-07-22 10:04:48 -0300

    Update UsuarioResponseDTO.java

--- src/main/java/br/com/agendamento/dto/in/UsuarioResponseDTO.java
+++ src/main/java/br/com/agendamento/dto/in/UsuarioResponseDTO.java
@@ -5,10 +5,14 @@ import lombok.Data;
 import javax.management.relation.Role;
 
 @Data
+@Schema(description="Classe response dto Usuario")
 public class UsuarioResponseDTO {
     private Long id;
+    @Schema(description="nome do Usuario", example="Matheus")
     private String nome;
+    @Schema(description="email de contato do usuario", examplo="matheus.usuario@gmail.com")
     private String email;
+    @Schema(description="senha para acesso do usuario", example="matheus123")
     private String senha;
     private Role role;
 }
