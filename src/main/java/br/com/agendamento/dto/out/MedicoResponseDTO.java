de9994f2f8b697ed30855e2d5af7601013dedffa
Christian Schunck de Almeida <christian.schunck10@gmail.com>
2026-07-22 12:45:29 -0300

    Update MedicoResponseDTO.java

--- src/main/java/br/com/agendamento/dto/out/MedicoResponseDTO.java
+++ src/main/java/br/com/agendamento/dto/out/MedicoResponseDTO.java
@@ -8,14 +8,14 @@ import lombok.Data;
 public class MedicoResponseDTO {
 
     private Long id;
-    @Schema(description="nome do Médico", example="Doutor Alexandre")
+    @Schema(description="nome do Médico", example="Doutor Alexandre", minLength= 3, maxLength= 50)
     private String nome;
-    @Schema(description="email de contato do Médico", example="alexandre.cardiologia@gmail.com")
+    @Schema(description="email de contato do Médico", example="alexandre.cardiologia@gmail.com", minLength= 15, maxLength= 50)
     private String email;
-    @Schema(description="area técnica de atuação", example="Cardiologia")
+    @Schema(description="area técnica de atuação", example="Cardiologia", minLength= 3, maxLength= 50)
     private String areaAtuacao;
-    @Schema(description="crm do profissional", example="394343")
+    @Schema(description="crm do profissional", example="394343", minLength= 6, maxLength= 25)
     private String crm;
-    @Schema(description="nivel de senioridade", example="junior/pleno/senior")
+    @Schema(description="nivel de senioridade", example="junior/pleno/senior", minLength= 5, maxLength= 25)
     private Senioridade senioridade;
 }
