b375a3aa3cbde18055f2bee78812dd4efc2d3cdb
Christian Schunck de Almeida <christian.schunck10@gmail.com>
2026-07-22 12:37:14 -0300

    Update AgendamentoExameResponseDTO.java

--- src/main/java/br/com/agendamento/dto/out/AgendamentoExameResponseDTO.java
+++ src/main/java/br/com/agendamento/dto/out/AgendamentoExameResponseDTO.java
@@ -12,14 +12,14 @@ import java.time.LocalDateTime;
 @Schema(description="classe response dto AgendamentoExame")
 public class AgendamentoExameResponseDTO {
     private Long id;
-    @Schema(description="nome do Médico agendado", example="Cardiologista", minlenght="3", maxlenght="30")
+    @Schema(description="nome do Médico agendado", example="Cardiologista", minLength= 3, maxLength= 30)
     private MedicoJava medicoJava;
-    @Schema(description="nome do Paciente agendado", example="Matheus")
+    @Schema(description="nome do Paciente agendado", example="Matheus", minLength= 3, maxLength= 50)
     private PacienteJava pacienteJava;
-    @Schema(description="horário agendado", example="14:00")
+    @Schema(description="horário agendado", example="14:00", minLength=4, maxLength=5)
     private LocalDateTime horarioAgendado;
-    @Schema(description="pedido de exames passado", example="Urina, abdomen total")
+    @Schema(description="pedido de exames passado", example="Urina, abdomen total", minLength= 5, maxLength=150)
     private ReceitaExamesJava receitaExamesJava;
-    @Schema(description="local de realizacao de exames", example="Hospital ALbert Eisten")
+    @Schema(description="local de realizacao de exames", example="Hospital ALbert Eisten", minLenght= 6, maxLength=50)
     private LaboratorioHospitalJava laboratorioHospitalJava;
 }
