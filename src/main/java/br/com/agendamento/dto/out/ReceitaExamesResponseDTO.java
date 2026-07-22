020820e9b238658014719f608ef152869eb97c59
Christian Schunck de Almeida <christian.schunck10@gmail.com>
2026-07-22 12:39:08 -0300

    Update ReceitaExamesResponseDTO.java

--- src/main/java/br/com/agendamento/dto/out/ReceitaExamesResponseDTO.java
+++ src/main/java/br/com/agendamento/dto/out/ReceitaExamesResponseDTO.java
@@ -6,8 +6,8 @@ import lombok.Data;
 @Schema(description="Classe response dto de receita de exames")
 public class ReceitaExamesResponseDTO {
     private Long id;
-    @Schema(description="pedido de exames", example="Urina, abdomen total")
+    @Schema(description="pedido de exames", example="Urina, abdomen total", minLength= 5, maxLength= 150)
     private String exame;
-    @Schema(description="descritivo do pedido de exames", example="Exame de urina, ir de bexiga cheia")
+    @Schema(description="descritivo do pedido de exames", example="Exame de urina, ir de bexiga cheia", minLength= 6, maxLength= 150)
     private String descricao;
 }
