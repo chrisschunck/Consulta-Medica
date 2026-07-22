183fe9710d7c538c2e36261f9527b11da2886583
Christian Schunck de Almeida <christian.schunck10@gmail.com>
2026-07-22 12:51:48 -0300

    Update ConvenioResponseDTO.java

--- src/main/java/br/com/agendamento/dto/out/ConvenioResponseDTO.java
+++ src/main/java/br/com/agendamento/dto/out/ConvenioResponseDTO.java
@@ -5,8 +5,8 @@ import lombok.Data;
 @Data
 public class ConvenioResponseDTO {
     private Long id;
-    @Schema(description="marca do plano de saúde", example="Porto Seguro")
+    @Schema(description="marca do plano de saúde", example="Porto Seguro", minLength= 5, maxLength= 25)
     private String marca;
-    @Schema(description="valor mensalidade", example="1.399")
+    @Schema(description="valor mensalidade", example="1.399", minLength= 3, maxLength= 10)
     private String valor;
 }
