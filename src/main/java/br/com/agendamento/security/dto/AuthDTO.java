bcad84ee3651537da8cc159a6d4b4f4f4babacfb
Christian Schunck de Almeida <christian.schunck10@gmail.com>
2026-07-22 10:12:33 -0300

    Update AuthDTO.java

--- src/main/java/br/com/agendamento/security/dto/AuthDTO.java
+++ src/main/java/br/com/agendamento/security/dto/AuthDTO.java
@@ -7,14 +7,14 @@ public class AuthDTO {
 
     @Schema(description="record request dto Login")
     public record LoginRequest(
-        @NotBlank(message = "Email e obrigatorio") @Email String email,
-        @NotBlank(message = "Senha e obrigatoria") String senha
+        @NotBlank(message = "Email e obrigatorio") @Email @Schema(description="email para login do usuario", example="matheus.usuario@gmail.com") String email,
+        @NotBlank(message = "Senha e obrigatoria") @Schema(description="senha para login do usuario", example="matheus123") String senha
     ) {}
 
     @Schema(description="record request dto Registro")
     public record RegistroRequest(
-        @NotBlank @Size(min = 3, max = 120) String nome,
-        @NotBlank @Email String email,
+        @NotBlank @Size(min = 3, max = 120) @Schema(description="nome completo do usuario", example="Matheus Henrique de Souza") String nome,
+        @NotBlank @Email @Schema(description="email de contato", example="matheus.usuario@gmail.com") String email,
         @NotBlank @Size(min = 6, message = "Senha deve ter no minimo 6 caracteres") String senha,
         @NotBlank String role
     ) {}
