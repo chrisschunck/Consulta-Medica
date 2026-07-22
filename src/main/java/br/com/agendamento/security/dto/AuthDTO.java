809c452d3a5434ccbdb59723c8bd1a6adddd54dd
Christian Schunck de Almeida <christian.schunck10@gmail.com>
2026-07-22 10:09:23 -0300

    Update AuthDTO.java

--- src/main/java/br/com/agendamento/security/dto/AuthDTO.java
+++ src/main/java/br/com/agendamento/security/dto/AuthDTO.java
@@ -2,13 +2,16 @@ package br.com.agendamento.security.dto;
 
 import jakarta.validation.constraints.*;
 
+@Schema(description="classe resquest e response dto Auth")
 public class AuthDTO {
 
+    @Schema(description="record request dto Login")
     public record LoginRequest(
         @NotBlank(message = "Email e obrigatorio") @Email String email,
         @NotBlank(message = "Senha e obrigatoria") String senha
     ) {}
 
+    @Schema(description="record request dto Registro")
     public record RegistroRequest(
         @NotBlank @Size(min = 3, max = 120) String nome,
         @NotBlank @Email String email,
@@ -16,5 +19,6 @@ public class AuthDTO {
         @NotBlank String role
     ) {}
 
+    @Schema(description="record response dto Token")
     public record TokenResponse(String token, String tipo, String email, String role) {}
 }
