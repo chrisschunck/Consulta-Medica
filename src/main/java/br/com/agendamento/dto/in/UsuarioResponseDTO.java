e6e6ee483c6662067cb04547f6f7401495006943
Christian Schunck de Almeida <christian.schunck10@gmail.com>
2026-07-22 13:02:55 -0300

    Update UsuarioResponseDTO.java

--- src/main/java/br/com/agendamento/dto/in/UsuarioResponseDTO.java
+++ src/main/java/br/com/agendamento/dto/in/UsuarioResponseDTO.java
@@ -8,11 +8,11 @@ import javax.management.relation.Role;
 @Schema(description="Classe response dto Usuario")
 public class UsuarioResponseDTO {
     private Long id;
-    @Schema(description="nome do Usuario", example="Matheus")
+    @Schema(description="nome do Usuario", example="Matheus", minLength= 3, maxLength= 50)
     private String nome;
-    @Schema(description="email de contato do usuario", examplo="matheus.usuario@gmail.com")
+    @Schema(description="email de contato do usuario", examplo="matheus.usuario@gmail.com", minLength= 15, maxLength= 50)
     private String email;
-    @Schema(description="senha para acesso do usuario", example="matheus123")
+    @Schema(description="senha para acesso do usuario", example="matheus123", minLength= 5, maxLength= 25)
     private String senha;
     private Role role;
 }
