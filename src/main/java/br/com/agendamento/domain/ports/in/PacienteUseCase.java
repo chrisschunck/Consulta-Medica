43e4bcd2fb716bf753b547386bc87b163e26ddad
Christian Schunck de Almeida <christian.schunck10@gmail.com>
2026-07-23 12:57:55 -0300

    Update PacienteUseCase.java

--- src/main/java/br/com/agendamento/domain/ports/in/PacienteUseCase.java
+++ src/main/java/br/com/agendamento/domain/ports/in/PacienteUseCase.java
@@ -5,6 +5,7 @@ import br.com.agendamento.domain.model.PacienteJava;
 import jakarta.validation.Valid;
 import java.util.List;
 
+@Component
 @Schema(description="classe Paciente para casos de uso")
 public interface PacienteUseCase {
 
