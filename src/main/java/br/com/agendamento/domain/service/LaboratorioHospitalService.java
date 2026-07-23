fb3b825e069cb8b0d2772203b57033fad793b895
Christian Schunck de Almeida <christian.schunck10@gmail.com>
2026-07-23 12:53:04 -0300

    Update LaboratorioHospitalService.java

--- src/main/java/br/com/agendamento/domain/service/LaboratorioHospitalService.java
+++ src/main/java/br/com/agendamento/domain/service/LaboratorioHospitalService.java
@@ -12,6 +12,7 @@ import java.util.NoSuchElementException;
 
 @Service
 @Validated
+@component
 @Schema(description="classe LaboratorioHospital implementado UseCase para regras de negócios")
 public class LaboratorioHospitalService implements LaboratorioHospitalUseCase {
 
