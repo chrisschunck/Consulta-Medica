cf3f112c72ca8f1c5ad490b8f5735e4fc5507669
Christian Schunck de Almeida <christian.schunck10@gmail.com>
2026-07-23 11:36:31 -0300

    Update LaboratorioHospitalJava.java

--- src/main/java/br/com/agendamento/domain/model/LaboratorioHospitalJava.java
+++ src/main/java/br/com/agendamento/domain/model/LaboratorioHospitalJava.java
@@ -24,7 +24,7 @@ public class LaboratorioHospitalJava {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", unique=true)
+    @Column(name = "id")
     private Long id;
 
     @Size(min = 2, max = 150, message = "Nome deve ter entre 2 e 150 caracteres")
