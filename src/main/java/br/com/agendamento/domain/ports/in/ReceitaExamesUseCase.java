d85c8f51e5108c2471ccf6860bae6c70109332ca
Christian Schunck de Almeida <christian.schunck10@gmail.com>
2026-07-23 12:58:07 -0300

    Update ReceitaExamesUseCase.java

--- src/main/java/br/com/agendamento/domain/ports/in/ReceitaExamesUseCase.java
+++ src/main/java/br/com/agendamento/domain/ports/in/ReceitaExamesUseCase.java
@@ -5,6 +5,7 @@ import br.com.agendamento.domain.model.ReceitaExamesJava;
 import jakarta.validation.Valid;
 import java.util.List;
 
+@Component
 @Schema(description="interface ReceitaExames para casos de uso")
 public interface ReceitaExamesUseCase {
 
