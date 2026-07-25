f0b7abb8f573a0a734b7d0373bd02f714d9ff8cc
Christian Schunck de Almeida <christian.schunck10@gmail.com>
2026-07-25 15:39:29 -0300

    Update ConvenioMedicoController.java

--- src/main/java/br/com/agendamento/adapters/in/web/ConvenioMedicoController.java
+++ src/main/java/br/com/agendamento/adapters/in/web/ConvenioMedicoController.java
@@ -20,21 +20,25 @@ public class ConvenioMedicoController {
         this.useCase = useCase;
     }
 
+    @Operation(summary="registra um convenio médico", description="salva os dados cadastrais do convenio médico")
     @PostMapping
     public ResponseEntity<ConvenioMedicoJava> salvar(@Valid @RequestBody ConvenioMedicoJava entidade) {
         return ResponseEntity.status(HttpStatus.CREATED).body(useCase.salvar(entidade));
     }
 
+    @Operation(summary="busca um convenio médico por id", description="retorna os dados cadastrais do convenio médico buscado por id")
     @GetMapping("/{id}")
     public ResponseEntity<ConvenioMedicoJava> buscarPorId(@PathVariable Long id) {
         return ResponseEntity.ok(useCase.buscarPorId(id));
     }
 
+    @Operation(summary="lista todos os convenios médicos", description="retorna uma lista de convenios médicos disponiveis")
     @GetMapping
     public ResponseEntity<List<ConvenioMedicoJava>> listarTodos() {
         return ResponseEntity.ok(useCase.listarTodos());
     }
 
+    @Operation(summary="deleta um convenio médico por id", description="remove os dados cadastrais do convenio médico")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletarPorId(@PathVariable Long id) {
         useCase.deletarPorId(id);
