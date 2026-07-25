ea80a2bc2052184b51432285824318d58bba3516
Christian Schunck de Almeida <christian.schunck10@gmail.com>
2026-07-25 15:32:18 -0300

    Update MedicoController.java

--- src/main/java/br/com/agendamento/adapters/in/web/MedicoController.java
+++ src/main/java/br/com/agendamento/adapters/in/web/MedicoController.java
@@ -20,21 +20,25 @@ public class MedicoController {
         this.useCase = useCase;
     }
 
+    @Operation(summary="registra um médico", description="salva os dados cadastrais de um médico")
     @PostMapping
     public ResponseEntity<MedicoJava> salvar(@Valid @RequestBody MedicoJava entidade) {
         return ResponseEntity.status(HttpStatus.CREATED).body(useCase.salvar(entidade));
     }
 
+    @Operation(summary="busca um médico por id", description="retorna os dados cadastrais do médico buscado")
     @GetMapping("/{id}")
     public ResponseEntity<MedicoJava> buscarPorId(@PathVariable Long id) {
         return ResponseEntity.ok(useCase.buscarPorId(id));
     }
 
+    @Operation(summary="lista todos os médicos", description="retorna uma lista de todos os médicos")
     @GetMapping
     public ResponseEntity<List<MedicoJava>> listarTodos() {
         return ResponseEntity.ok(useCase.listarTodos());
     }
 
+    @Operation(summary="deleta um médico por id", description="deleta os dados cadastrais do médico removido")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletarPorId(@PathVariable Long id) {
         useCase.deletarPorId(id);
